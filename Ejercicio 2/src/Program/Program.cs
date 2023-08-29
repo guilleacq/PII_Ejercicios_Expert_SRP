17fa5ada7989a7aaa6d43c64a40b6dc8a5cc8344
Guillermo Acquistapace <guillermoacq@gmail.com>
2023-08-29 16:56:35 -0300

    Added a client class

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,10 +7,12 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
+            Client client1 = new Client("Steven Jhonson", "986782342", "5555-555-555");
+            string appointmentResult = AppointmentService.CreateAppointment(client1, DateTime.Now, "Wall Street", "Armand");
             Console.WriteLine(appointmentResult);
 
-            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
+            Client client2 = new Client("Ralf Manson", "", "5555-555-555");
+            string appointmentResult2 = AppointmentService.CreateAppointment(client2, DateTime.Now, "Queen Street", "");
             Console.WriteLine(appointmentResult2);
         }
     }
