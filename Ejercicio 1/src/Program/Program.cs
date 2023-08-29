34c3b724bd27848f85eef4659534565e197533a3
Guillermo Acquistapace <guillermoacq@gmail.com>
2023-08-29 16:10:22 -0300

    Added a way to shelve books and improved overall code

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -6,11 +6,13 @@ namespace SRP
     {
         static void Main()
         {
-            Sector sectorNovelas = new Sector("Novelas");
+            Sector sectorNovelas = new Sector("Novelas", 3);
+
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
-            //Book book2 = new Book("Pro C#","Troelsen","001-035");
-            sectorNovelas.ShelveBook(book1, 2);
-            //book2.ShelveBook("B","3");
+            Book book2 = new Book("Pro C#","Troelsen","001-035");
+
+            sectorNovelas.ShelveBook(book1, 1);
+            sectorNovelas.ShelveBook(book2 , 1);
 
         }
     }
